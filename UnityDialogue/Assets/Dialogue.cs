c2e0b8089d5b4ffee382b683e61759f91184f0f2
Eric <EricMouledoux@Gmail.com>
2016-06-09 15:12:12 -0500

    dialogue redo
    -redid class to handle all outputs internally
    -keeps track of current person talking and phrase
    -reads in conversation from text file

---
+++ UnityDialogue/Assets/Dialogue.cs
@@ -0,0 +1,40 @@
+﻿namespace Conversation
+{
+    public class Conversation
+    {
+        public Conversation(string a_FilePath)
+        {
+            lines = new System.Collections.Generic.List<string>();
+            foreach (string s in System.IO.File.ReadAllLines(a_FilePath))
+            {
+                if (!s.Contains(@"//") && s.Length > 0)
+                {
+                    lines.Add(s);
+                }
+            }
+            currentIndex = 0;
+            totalPhrases = lines.Count/2;
+        }
+
+        public string GetCurrentNameAndPhrase()
+        {
+            string output = GetCurrentPersonSpeaking() + "\n" +
+                            GetCurrentPhrase();
+            return output;
+        }
+
+        public string GetCurrentPersonSpeaking()
+        {
+            return lines[currentIndex * 2];
+        }
+
+        public string GetCurrentPhrase()
+        {
+            return lines[(currentIndex * 2) + 1];
+        }
+
+        private System.Collections.Generic.List<string> lines;
+        public int currentIndex;
+        public int totalPhrases;
+    }
+}
---
+++ UnityDialogue/Assets/TalkingBox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkingBox : MonoBehaviour
+{
+    Conversation.Conversation m_Dialogue = new Conversation.Conversation(@".\Assets\Conversation.txt");
+
+    void Start()
+    {
+        Debug.Log(m_Dialogue.totalPhrases);
+    }
+
+    public Text textBox;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (m_Dialogue.currentIndex >= m_Dialogue.totalPhrases)
+            {
+                textBox.text = "";
+                return;
+            }
+            textBox.text = m_Dialogue.GetCurrentNameAndPhrase();
+            m_Dialogue.currentIndex++;
+        }
+    }
+}
